ed702861b8ec2cdc2465172f7acfa82ad08f491a
Ashvin Warrier <warrierar@gmail.com>
2019-10-30 18:49:18 -0400

    Fresh start

--- Terrain.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import settings
-
-
-class Terrain(pygame.Rect):
-    def __init__(self):
-        super().__init__(0, settings.height * 0.9, settings.width, 2)
-        print(settings.size)
